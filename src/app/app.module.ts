2012464e6eaee7b5c33685756146d9788a1b3c52
Berkay Anduv <sygrok@hotmail.com>
2024-01-06 03:27:27 +0300

    revize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { SertificatesComponent } from './components/sertificates/sertificates.component';
-import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutMeComponent,
-    ProjectsComponent,
-    SertificatesComponent,
-    EducationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

a8a8232e11200cd28403b753ed68d08b042a4412
Berkay Anduv <sygrok@hotmail.com>
2024-01-17 05:48:53 +0300

    CertificatesSection

--- src/app/components/certificates-section/certificates-section.component.ts
+++ src/app/components/certificates-section/certificates-section.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppModalComponent } from '../app-modal/app-modal.component';
+import { Certificates } from '../../interfaces/certificates';
 
 @Component({
   selector: 'app-certificates-section',
@@ -10,67 +11,78 @@ import { AppModalComponent } from '../app-modal/app-modal.component';
 export class CertificatesSectionComponent {
   constructor(private modalService: NgbModal) {}
 
-  certificates: [] = [
+  certificates: Certificates[] = [
     {
       id: 1,
-      logo: 'fa-brands fa-angular',
+      logo: 'fa-brands fa-react',
       name: `React - The Complete Guide (Academind by
       Maximilian Schwarzmüller) - Udemy`,
       img: '../assets/sertificates/ReactComplete.png',
     },
     {
       id: 2,
+      logo: 'fa-brands fa-angular',
       name: `Frontend Web Development Training - Ministry of Labour and Social
         Security`,
       img: '../assets/sertificates/AngularBootcamp.png',
     },
     {
       id: 3,
+      logo: 'fa-brands fa-angular',
       name: `Angular 401 - Turkcell Academy`,
       img: '../assets/sertificates/angular401.png',
     },
     {
       id: 4,
+      logo: 'fa-brands fa-angular',
       name: `Angular 301 - Turkcell Academy`,
       img: '../assets/sertificates/angular301.png',
     },
     {
       id: 5,
+      logo: 'fa-brands fa-angular',
       name: `Angular 201 - Turkcell Academy`,
       img: '../assets/sertificates/angular201.png',
     },
     {
       id: 6,
+      logo: 'fa-brands fa-angular',
       name: `Angular 101 - Turkcell Academy`,
       img: '../assets/sertificates/angular101.png',
     },
     {
       id: 7,
+      logo: 'fa-brands fa-react',
       name: `React 101 - Turkcell Academy`,
       img: '../assets/sertificates/react101.png',
     },
     {
       id: 8,
+      logo: 'fa-brands fa-js',
       name: `Introduction to JavaScript - Sololearn`,
       img: '../assets/sertificates/Sololearn JavaScript.jpeg',
     },
     {
       id: 9,
+      logo: 'fa-brands fa-css3-alt',
       name: `Introduction to CSS - Sololearn`,
       img: '../assets/sertificates/Sololearn CSS.jpeg',
     },
     {
       id: 10,
+      logo: 'fa-brands fa-html5',
       name: `HTML5 & CSS3 - Turkcell Academy`,
       img: '../assets/sertificates/html5&css.png',
     },
     {
       id: 11,
+      logo: 'fa-brands fa-html5',
       name: `HTML 101 - Turkcell Academy`,
       img: '../assets/sertificates/html101.png',
     },
     {
       id: 12,
+      logo: 'fa-solid fa-microchip',
       name: `Introduction to Information Technologies -
       BTK Academy
       `,
--- src/app/interfaces/certificates.ts
+++ src/app/interfaces/certificates.ts
@@ -1,2 +1,6 @@
 export interface Certificates {
+  id: number;
+  logo: string;
+  name: string;
+  img: string;
 }
